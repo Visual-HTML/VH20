0ae69966acdd176a467bcbaa44bb8b43f7c64145
Visual-HTML <visualhtml@gmail.com>
2017-09-22 22:34:11 +0200

    Update VH20.js

--- VH20.js
+++ VH20.js
@@ -3,6 +3,7 @@ VH20 = {};
 VH20.Script.Import = function(key) {};
 VH20.Script.Reference = function(url) {};
 VH20.Script.Unload = function(url) {};
+
 VH20.Browser = {};
 VH20.Browser.Class = "default";
 VH20.BrowserClass = function() {
@@ -30,33 +31,12 @@ VH20.BrowserClass = function() {
 };
 VH20.BrowserClass();
 
-VH20.InitializeEvent = function() {};
-
-// load VH20' browser specific code
-if (VH20.Browser.Class === "MSIE8") {
-	VH20.IncludeDynamicScriptAfter("https://visual-html.github.io/V-HTML/VH20-MSIE8.js",
-		document.getElementsByTagName("head")[0].querySelector('script[src*="VH20.js"]')); } 
-else if (VH20.Browser.Class === "MSIE10") {
-	VH20.IncludeDynamicScriptAfter("https://visual-html.github.io/V-HTML/VH20-MSIE10.js",
-		document.getElementsByTagName("head")[0].querySelector('script[src*="VH20.js"]')); } 
-else if (VH20.Browser.Class === "MSIE11") {
- 	VH20.IncludeDynamicScriptAfter("https://visual-html.github.io/V-HTML/VH20-MSIE11.js",
-	 	document.getElementsByTagName("head")[0].querySelector('script[src*="VH20.js"]')); }
-else if (VH20.Browser.Class === "Opera") {
- 	VH20.IncludeDynamicScriptAfter("https://visual-html.github.io/V-HTML/VH20-Opera.js",
-	 	document.getElementsByTagName("head")[0].querySelector('script[src*="VH20.js"]')); }
-else if (VH20.Browser.Class === "Netscape") {
-	 VH20.IncludeDynamicScript("https://visual-html.github.io/V-HTML/VH20-Netscape.js",
-	 	document.getElementsByTagName("head")[0].querySelector('script[src*="VH20.js"]')); }
-else if (VH20.Browser.Class === "Firefox") {
- 	VH20.IncludeDynamicScriptAfter("https://visual-html.github.io/V-HTML/VH20-Firefox.js",
-		document.getElementsByTagName("head")[0].querySelector('script[src*="VH20.js"]')); }
-else if (VH20.Browser.Class === "Chrome") {
-	VH20.IncludeDynamicScriptAfter("https://visual-html.github.io/V-HTML/VH20-Chrome.js",
-		document.getElementsByTagName("head")[0].querySelector('script[src*="VH20.js"]'));
-}
 
 
-VH20.InitializeEvent();
+VH20.Initialize = function() {};
+
+
+
+VH20.Initialize();
 
 
