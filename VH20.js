af19a54f3c3272aefe334283cbf8708bc5716423
Visual-HTML <visualhtml@gmail.com>
2018-03-06 02:46:47 +0100

    Need a VH20 proposal
    What does it ?

--- VH20.js
+++ VH20.js
@@ -1,2 +1,13 @@
-VH20 = {};
+VH20 = {  // an object literal or a function ?
 
+// a data store, empty populated with key:user-agent value:list of js file
+  
+function Processing() {
+  // cross-browser code for dynamic inclusion of js files
+  
+  // there is a loop..
+}
+  
+function Run() { this.Processing() } 
+
+};
